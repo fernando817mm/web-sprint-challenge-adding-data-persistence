0c5ef65ede52bfa2409c0835b2121d20230fd563
Fernando Martinez <fernando817mm@gmail.com>
2021-09-10 16:27:24 -0500

    finalllllly done

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,22 @@
-// build your `Task` model here
+const db = require("./../../data/dbConfig");
+
+const getAll = () => {
+  return db("tasks")
+    .leftJoin("projects", "projects.project_id", "tasks.project_id")
+    .select("tasks.*", "projects.project_name", "projects.project_description");
+};
+
+const getById = (id) => {
+  return db("tasks").where("task_id", id).first();
+};
+
+const insert = async (task) => {
+  const [id] = await db("tasks").insert(task);
+  return getById(id);
+};
+
+module.exports = {
+  getAll,
+  getById,
+  insert,
+};
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,27 @@
-// build your `/api/tasks` router here
+const router = require("express").Router();
+const Task = require("./model");
+
+router.get("/", (req, res, next) => {
+  Task.getAll()
+    .then((tasks) => {
+      tasks.map((task) => {
+        task.task_completed = task.task_completed === 0 ? false : true;
+      });
+      res.json(tasks);
+    })
+    .catch(next);
+});
+
+router.post("/", (req, res, next) => {
+  Task.insert(req.body)
+    .then((task) => {
+      const newTask = {
+        ...task,
+        task_completed: Boolean(Number(task.task_completed)),
+      };
+      res.json(newTask);
+    })
+    .catch(next);
+});
+
+module.exports = router;
